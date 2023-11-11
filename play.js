0ffe0238f1ebb293de3156215a3c32e6f3815129
Haylie Jarvis <hjballet@yahoo.com>
2023-11-11 15:11:24 -0700

    actually linked js to html

--- play.js
+++ play.js
@@ -1,7 +1,14 @@
+const opt1 = document.getElementById('opt1');
+  const opt2 = document.getElementById('opt2');
+  const opt3 = document.getElementById('opt3');
+  const opt4 = document.getElementById('opt4');
+  const opt5 = document.getElementById('opt5');
+
 function submit() {
     const nameEl = document.querySelector("#count");
     localStorage.setItem("question", nameEl.value);
     displayQuestion();
+    opt1.focus();
   }
 
   function displayQuestion() {
@@ -16,14 +23,17 @@ function submit() {
   window.onload = displayQuestion;
 
 
-  const opt1 = document.getElementById('opt1');
-  const opt2 = document.getElementById('opt2');
-  const opt3 = document.getElementById('opt3');
-  const opt4 = document.getElementById('opt4');
-  const opt5 = document.getElementById('opt5');
-
     // Add a blur event listener to the first textbox
   opt1.addEventListener('blur', function () {
     // Automatically move focus to the second textbox
     opt2.focus();
+  });
+  opt2.addEventListener('blur', function () {
+    opt3.focus();
+  });
+  opt3.addEventListener('blur', function () {
+    opt4.focus();
+  });
+  opt4.addEventListener('blur', function () {
+    opt5.focus();
   });
